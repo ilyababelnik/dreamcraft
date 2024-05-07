65eec483f29cf28978cc22684309d90c4508a235
Illia Babelnyk <ilyaakulov96@gmail.com>
2024-05-07 19:48:24 +0300

    fix: swagger documentation

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'Swagger',
+                'title' => 'documentation',
             ],
 
             'routes' => [
