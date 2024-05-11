9f498c778c088a821c0325727c660d0e3e916068
Illia Babelnyk <ilyaakulov96@gmail.com>
2024-05-11 20:26:42 +0300

    fix: add avatar field to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'first_name',
         'last_name',
         'nickname',
+        'avatar',
         'email',
         'email_verified_at',
         'password',
